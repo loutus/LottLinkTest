70d0b7c752faf7f90cb502d8a685ee19a5205cab
r.nouripoor <r.nouripoor@gmail.com>
2021-11-13 16:52:37 +0330

    TEST_1.0.5.3

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -3,24 +3,24 @@ const { ethers } = require("hardhat");
 async function main() {
     const [deployer] = await ethers.getSigners();
   
-    console.log("Owner address:", deployer.address);
+    // console.log("Owner address:", deployer.address);
 
-    const Register = await ethers.getContractFactory("Register");
-    const R = await Register.deploy("0x0000000000000000000000000000000000000000", "100000000000000000");
-    console.log("Register Contract Address:", R.address);
+    // const Register = await ethers.getContractFactory("Register");
+    // const R = await Register.deploy("0x0000000000000000000000000000000000000000", "100000000000000000");
+    // console.log("Register Contract Address:", R.address);
 
-    const Consumer = await ethers.getContractFactory("RandomNumberConsumer");
-    const RNC = await Consumer.deploy();
-    console.log("Random Number Consumer Contract Address:", RNC.address);
+    // const Consumer = await ethers.getContractFactory("RandomNumberConsumer");
+    // const RNC = await Consumer.deploy();
+    // console.log("Random Number Consumer Contract Address:", RNC.address);
 
 
-    const ChanceRoom = await ethers.getContractFactory("ChanceRoom");
-    const ChR = await ChanceRoom.deploy();
-    console.log("ChanceRoom Library Contract Address:", ChR.address);
+    // const ChanceRoom = await ethers.getContractFactory("ChanceRoom");
+    // const ChR = await ChanceRoom.deploy();
+    // console.log("ChanceRoom Library Contract Address:", ChR.address);
 
 
     const Factory = await ethers.getContractFactory("Factory");
-    const F = await Factory.deploy(R.address, RNC.address, ChR.address);
+    const F = await Factory.deploy("0x92c3f3b2122b61a50b218df446e2799535fcb519", "0xa175b2bc8028286a1cb68c06c9fc0f57deccc378", "0x3c028e8b052e83d8215c13a16699eac8564432d9", "0x043ca25f2c83862b30eb128d25adc9239464d985");
     console.log("Factory Contract Address:", F.address);
   }
   
